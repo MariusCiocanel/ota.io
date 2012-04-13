7c26a0420f8dd497040526084c365a91de613320
Nolan B <nolanbrown@gmail.com>
2012-04-12 21:00:41 -0700

    Updated production URL

--- application.rb
+++ application.rb
@@ -13,7 +13,7 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/appsendr
 if development
     BASE_URL = "http://127.0.0.1:9393"
 else 
-    BASE_URL = "http://appsendr.com"
+    BASE_URL = "http://appsendr-v2.heroku.com"
 end
 S3_KEY     = 'AKIAI25H5FVEUIGMP6YQ'
 S3_SECRET = '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'
